bb8786e1a043a6ddf9d012dcea8995f4aea69ec7
Ray Lugo, Jr <rlugojr@gmail.com>
2017-08-03 16:14:43 -0400

    Added tests for views
    Added select statements to test views after creation.

--- sql_views.sql
+++ sql_views.sql
@@ -5,3 +5,9 @@ create view authors_most_popular as select name, count(*) as total_reads from (s
 create view request_high_percent_errors as
 select log_date, percent_errors from (select treads.log_date as log_date, total_reads, total_failures, (cast(total_failures as float)/cast(total_reads as float) * 100) as percent_errors from (select to_char(time,'YYYY MM DD') as log_date, count(*) as total_reads from log where status = '200 OK'group by log_date) as treads
 join (select to_char(time,'YYYY MM DD') as log_date, count(*) as total_failures from log where status = '404 NOT FOUND' group by log_date) as tfails on treads.log_date = tfails.log_date) as report where percent_errors > 1;
+
+select * from articles_most_popular;
+
+select * from authors_most_popular;
+
+select * from request_high_percent_errors;
