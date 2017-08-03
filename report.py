55a171fd0a0c1c831810cfb6ee6c44d5bd14670b
Ray Lugo, Jr <rlugojr@gmail.com>
2017-08-03 18:32:01 -0400

    Report update
    Completed SQL results sections and writing results to file.

--- report.py
+++ report.py
@@ -4,54 +4,91 @@ import datetime
 import sys
 
 DBNAME = 'news'
+FILENAME = 'Log_Report_{}.txt'.format(datetime.date.today())
+SPACER = '\n\n'
 
 def connect_to_db():
     """Establishes connection to SQL DB and returns connection and cursor objects."""
 
     try:
-        conn = psycopg2.connect(database = DBNAME)
+        conn = psycopg2.connect(database=DBNAME)
         cur = conn.cursor()
-        return conn, cur
+        return conn,cur
     except psycopg2.Error as e:
         print("Can't connect to {}", DBNAME)
         sys.exit(1)
 
 def get_results(sql_query):
     """Executes SQL Query provided as a parameter and returns a result object."""
-    
+
     try:
-        conn, cur = connect()
+        conn, cur = connect_to_db()
         cur.execute(sql_query)
         results = cur.fetchall()
         conn.close()
         return results
     except:
-        print("Failed to get results from query - {}", sql_query)
+        print("Failed to get results from query - {}".format(sql_query))
         sys.exit(1)
 
+def write_to_file(stringOut,mode):
+    """Placeholder"""
+    try:
+        with open(FILENAME, mode) as report_file:
+            report_file.write(stringOut)
+    except:
+        print("Error writing to text file.")
+
 def articles_most_popular():
+    """Placeholder"""
+    section_header = "The most popular three articles of all time:\n"
+    print(section_header)
+    write_to_file(section_header,'a')
+    items = get_results('select * from articles_most_popular')
+    for item in items:
+        line_out = '"{}" - {:,d} views\n'.format(item[0], item[1])
+        print(line_out)
+        write_to_file(line_out,'a')
+    print(SPACER)
+    write_to_file(SPACER,'a')
 
 
 def authors_most_popular():
-
+    """Placeholder"""
+    section_header = "The most popular article authors of all time:\n"
+    print(section_header)
+    write_to_file(section_header,'a')
+    items = get_results('select * from authors_most_popular')
+    for item in items:
+        line_out = '{} - {:,d} views\n'.format(item[0], item[1])
+        print(line_out)
+        write_to_file(line_out,'a')
+    print(SPACER)
+    write_to_file(SPACER,'a')
 
 def request_high_percent_errors():
-
+    """Placeholder"""
+    section_header = "The day with >1% request errors:\n"
+    print(section_header)
+    write_to_file(section_header,'a')
+    items = get_results('select * from request_high_percent_errors')
+    for item in items:
+        line_out = '{} - {:01.2f}% errors\n'.format(item[0], item[1])
+        print(line_out)
+        write_to_file(line_out,'a')
+    print(SPACER)
+    write_to_file(SPACER,'a')
 
 def build_report():
-    print("Log Analysis report - {}.", datetime.date.today())
+    header = "Log Analysis report - {}.\n\n".format(datetime.date.today())
+    print(header)
+    write_to_file(header,'w')
 
-    print("\n\nThe top three articles are:\n")
     articles_most_popular()
 
-    print("\n\nThe top authors are:\n")
     authors_most_popular()
 
-    print("\n\nDays with more than 1 Percent errors:\n")
     request_high_percent_errors()
-
-    print("\n\n")
-
 
 if __name__ == '__main__':
     build_report()
