08d878e3b9a56af309992e03e853155abf81a99e
Ray Lugo, Jr <rlugojr@gmail.com>
2017-08-03 16:25:35 -0400

    initial report.py
    initial commit of report.py with placeholders.

---
+++ report.py
@@ -0,0 +1,19 @@
+
+import psycopg
+
+DBNAME = 'news'
+
+def articles_most_popular():
+
+
+def authors_most_popular():
+
+
+def request_high_percent_errors():
+
+
+def build_report():
+
+
+if __name__ == '__main__':
+    build_report()
