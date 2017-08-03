aac8447de4e686ad474f713dad5befb5e37a28c9
Ray Lugo, Jr <rlugojr@gmail.com>
2017-08-03 15:59:20 -0400

    modifed sql_views
    Modified authors_most_popular view to order by column alias instead of
    aggregate count operator.

--- sql_views.sql
+++ sql_views.sql
@@ -1,6 +1,6 @@
 create view articles_most_popular as select title, count(*) as total_reads from (select a.title, a.slug, l.path, l.slug from articles as a join (select path, replace(path,'/article/','') as slug from log) as l on a.slug = l.slug) as article_reads group by title order by total_reads desc limit 3;
 
-create view authors_most_popular as select name, count(*) from (select au.name,au.id,a.author, a.title, a.slug, l.path, l.slug from authors as au join articles as a on au.id = a.author join (select path, replace(path,'/article/','') as slug from log) as l on a.slug = l.slug) as author_reads group by name order by count(*) desc;
+create view authors_most_popular as select name, count(*) as total_reads from (select au.name,au.id,a.author, a.title, a.slug, l.path, l.slug from authors as au join articles as a on au.id = a.author join (select path, replace(path,'/article/','') as slug from log) as l on a.slug = l.slug) as author_reads group by name order by total_reads desc;
 
 create view request_high_percent_errors as
 select log_date, percent_errors from (select treads.log_date as log_date, total_reads, total_failures, (cast(total_failures as float)/cast(total_reads as float) * 100) as percent_errors from (select to_char(time,'YYYY MM DD') as log_date, count(*) as total_reads from log where status = '200 OK'group by log_date) as treads
