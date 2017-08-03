ef6b14a85959a3e5f254f72bd9f924ef40b2d1e2
Ray Lugo, Jr <rlugojr@gmail.com>
2017-08-03 16:57:23 -0400

    Added connection function
    Added function that creates connection and cursor to DB and returns both
    to caller.  Handles exception with try-except.

--- report.py
+++ report.py
@@ -1,7 +1,20 @@
 
-import psycopg
+import psycopg2
+import datetime
+import sys
 
 DBNAME = 'news'
+
+def connect_to_db():
+    """Establishes connection to SQL DB and returns connection and cursor objects."""
+
+    try:
+        conn = psycopg2.connect(database = DBNAME)
+        cur = conn.cursor()
+        return conn, cur
+    except psycopg2.Error as e:
+        print("Can't connect to {}", DBNAME)
+        sys.exit(1)
 
 def articles_most_popular():
 
@@ -13,6 +26,18 @@ def request_high_percent_errors():
 
 
 def build_report():
+    print("Log Analysis report - {}.", datetime.date.today())
+
+    print("\n\nThe top three articles are:\n")
+    articles_most_popular()
+
+    print("\n\nThe top authors are:\n")
+    authors_most_popular()
+
+    print("\n\nDays with more than 1 Percent errors:\n")
+    request_high_percent_errors()
+
+    print("\n\n")
 
 
 if __name__ == '__main__':
