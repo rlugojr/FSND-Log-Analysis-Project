af1e378f59d8864f1767e8ae3a2ebb70deceb29d
Ray Lugo, Jr <rlugojr@gmail.com>
2017-08-04 09:54:33 -0400

    Reformat query
    reformatted query to multiline for readability.
    optimized first query as per Reviewer notes.

--- sql_views.sql
+++ sql_views.sql
@@ -1,5 +1,13 @@
-create view articles_most_popular as select a.title, count(*) as total_reads from articles as a join (select path, replace(path,'/article/','') as slug from log) as l
-on a.slug = l.slug group by a.title order by total_reads desc limit 3;
+create view articles_most_popular
+as
+SELECT a.title, count(*) AS total_reads
+   FROM articles AS a JOIN (
+     SELECT path
+     FROM log) AS l
+   ON '/article/' || a.slug = l.path
+   GROUP BY a.title
+   ORDER BY total_reads DESC
+   LIMIT 3;
 
 create view authors_most_popular as select au.name,count(*) as total_reads from authors as au join articles as a on au.id = a.author join (select path, replace(path,'/article/','') as slug from log) as l on a.slug = l.slug group by au.name order by total_reads desc;
 
@@ -10,4 +18,3 @@ select * from articles_most_popular;
 select * from authors_most_popular;
 
 select * from request_high_percent_errors;
-
