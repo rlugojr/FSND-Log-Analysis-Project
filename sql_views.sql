98b158ab251865acbb785e2871ddeb9de147e5df
Ray Lugo, Jr <rlugojr@gmail.com>
2017-08-04 00:33:29 -0400

    Optimized SQL Queries
    Optimized SQL queries by refactoring to use better aggregation functions
    and less sub queries.

--- sql_views.sql
+++ sql_views.sql
@@ -1,14 +1,13 @@
-create view articles_most_popular as select title, count(*) as total_reads from (select a.title, a.slug, l.path, l.slug from articles as a join (select path, replace(path,'/article/','') as slug from log) as l on a.slug = l.slug) as article_reads group by title order by total_reads desc limit 3;
+create view articles_most_popular as select a.title, count(*) as total_reads from articles as a join (select path, replace(path,'/article/','') as slug from log) as l
+on a.slug = l.slug group by a.title order by total_reads desc limit 3;
 
-create view authors_most_popular as select name, count(*) as total_reads from (select au.name,au.id,a.author, a.title, a.slug, l.path, l.slug from authors as au join articles as a on au.id = a.author join (select path, replace(path,'/article/','') as slug from log) as l on a.slug = l.slug) as author_reads group by name order by total_reads desc;
+create view authors_most_popular as select au.name,count(*) as total_reads from authors as au join articles as a on au.id = a.author join (select path, replace(path,'/article/','') as slug from log) as l on a.slug = l.slug group by au.name order by total_reads desc;
 
-create view request_high_percent_errors as select to_char(log_date,'YYYY-MM-DD') as log_date, percent_errors from (select treads.log_date as log_date, total_reads, total_failures, (cast(total_failures as float)/cast(total_reads as float) * 100) as percent_errors from (select cast(time as date) as log_date, count(*) as total_reads from log where status = '200 OK'group by log_date) as treads
-join (select cast(time as date) as log_date, count(*) as total_failures from log where status = '404 NOT FOUND' group by log_date) as tfails on treads.log_date = tfails.log_date) as report where percent_errors > 1;
+create view request_high_percent_errors as select log_date, ((cast(total_failures as float) / cast(total_reqs as float)) * 100) as error_percentage from (select cast(time as date) as log_date, count(*) as total_reqs, count(*) FILTER (WHERE status = '404 NOT FOUND') as total_failures from log group by log_date) as log_info where ((cast(total_failures as float) / cast(total_reqs as float)) * 100) > 1;
 
 select * from articles_most_popular;
 
 select * from authors_most_popular;
 
 select * from request_high_percent_errors;
-
 
